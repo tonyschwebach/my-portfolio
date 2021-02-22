bc730b07c38ce91cf7e92f2d5d6fdd02fa60d18b
T Schway <tony.schwebach@gmail.com>
2021-02-21 20:58:42 -0500

    formatted layout of resume and skills on about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import Bio from "../../components/Bio/Bio";
 import Skills from "../../components/Skills/Skills";
-import resume from "../../assets/resume.pdf"
+import resume from "../../assets/resume.pdf";
 import "./About.css";
 
 const About = () => {
   return (
     <div className="container">
       <Bio />
-      <Skills />
-      <h2 className="mt-3">Resume</h2>
-      <a href={resume}><i className="fas fa-file fa-4x skills-icon"></i></a>
-      <p className="text-center mb-5">Resume</p>
+      <div className="row mt-3 justify-content-around">
+        <div className="col-md-3">
+          <h2 className="mt-3">Resume</h2>
+          <a href={resume}>
+            <i className="fas fa-file fa-4x skills-icon"></i>
+          </a>
+          <p className="text-center mb-5">Resume</p>
+        </div>
+        <div className="col-md-8 px-4">
+          <Skills />
+        </div>
+      </div>
     </div>
   );
 };
